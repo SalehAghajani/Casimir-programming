0cbffddbb9cfbbb128b2f94c51c9f0f60eda88cc
SalehAghajani <S.Aghajani@tudelft.nl>
2021-01-25 13:51:14 +0100

    debugged

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ print('Hello World')
 
 
 import numpy as np
-def circumstance_circle(radius)
+def circumstance_circle(radius):
 	return 2*np.pi*raius
 
-def area_circle(radius)
+def area_circle(radius):
 	return np.pi*(radius**2)
 
 
