396e33fdff771e1bc80ed3f72ed7f7360ff4bbf9
SalehAghajani <S.Aghajani@tudelft.nl>
2021-01-25 14:00:47 +0100

    added test to script

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
 print("hello World")
 print("I am Saleh Aghajani")
-Print("And I am here try not to sleep")
+print("And I am here try not to sleep")
+
+
+from test import area_circle
+
+print(area_circle(5))
+
+
+
